531bb1182ea5d0c00ac13e1804a6a75c62e629a6
Luis Portillo <devcorvus.contact@gmail.com>
2022-10-22 22:23:32 -0400

    fix: change title when not authorized

--- apps/frontend/src/components/NotAuthorized.vue
+++ apps/frontend/src/components/NotAuthorized.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { useTitle } from '@vueuse/core';
+
+import { TITLE_SUFFIX } from '@utils/constants';
+
+useTitle('Not authorized' + TITLE_SUFFIX);
+</script>
+
 <template>
   <div
     class="absolute top-0 left-0 w-screen h-screen bg-slate-100 dark:bg-slate-900"
